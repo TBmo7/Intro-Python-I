cc4272a6354fd185da713d8d81217c61acdc33ac
Troy Moore <tbmoore85@gmail.com>
2020-07-15 21:17:20 -0400

    Finished wiht homework

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -10,7 +10,7 @@ for more info regarding list comprehensions.
 
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
-#y = []
+#y = [a for a in range(6) if a > 0]
 
 #for x in range(6): 
 #    if x > 0:
@@ -21,7 +21,7 @@ for more info regarding list comprehensions.
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
-#y = []
+#y = [a**3 for a in range(10)]
 
 #for x in range(10):
 #    y.append(x**3)
@@ -31,19 +31,22 @@ for more info regarding list comprehensions.
 # Write a list comprehension to produce the uppercase version of all the
 # elements in array a. Hint: "foo".upper() is "FOO".
 
-a = ["foo", "bar", "baz"]
+#a = ["foo", "bar", "baz"]
 
-y = [element.upper() for element in a]
+#y = [element.upper() for element in a]
 
-print(y)
+#print(y)
 
 # Use a list comprehension to create a list containing only the _even_ elements
 # the user entered into list x.
 
-x = input("Enter comma-separated numbers: ").split(',')
+#x = input("Enter comma-separated numbers: ").split(',')
 
 # What do you need between the square brackets to make it work?
-y = []
+#y = [a for a in x if a % 2 == 0]
+#for a in range(x):
+#    if a % 2 == 0:
+#        y.append(a)
 
-print(y)
+#print(y)
 
--- src/10_functions.py
+++ src/10_functions.py
@@ -1,12 +1,26 @@
 # Write a function is_even that will return true if the passed-in number is even.
 
 # YOUR CODE HERE
+def is_even(input):
+    if input % 2 == 0:
+        return True
+    else: 
+        return False
 
 # Read a number from the keyboard
 num = input("Enter a number: ")
 num = int(num)
 
+
+
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
 
+def is_true(input):
+    if input % 2 == 0:
+        print('Even!')
+    else: 
+        print('Odd')
+
+is_true(num)
